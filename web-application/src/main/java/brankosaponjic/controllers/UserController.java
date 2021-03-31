ed74338aa88604e2d17b1d25557e5af0066e4b85
Branko <brankosaponjic76@gmail.com>
2021-03-31 15:55:13 +0200

    Maven Dependencies Between Modules

---
+++ web-application/src/main/java/brankosaponjic/controllers/UserController.java
@@ -0,0 +1,12 @@
+package brankosaponjic.controllers;
+
+import brankosaponjic.domain.UserCommand;
+import brankosaponjic.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+        // fake impl
+        return new User();
+    }
+}
