71a924351e9d1c03d35fa4983954a8949b7d2eb3
Branko <brankosaponjic76@gmail.com>
2021-04-01 17:00:22 +0200

    Maven Enforcer Plugin

--- converters/target/generated-sources/annotations/brankosaponjic/converters/UserMapperImpl.java
+++ converters/target/generated-sources/annotations/brankosaponjic/converters/UserMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-01T13:48:54+0200",
+    date = "2021-04-01T16:55:26+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.10 (Oracle Corporation)"
 )
 public class UserMapperImpl implements UserMapper {
